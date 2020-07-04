2443cb4730e9d24131c73d1bd7a043e5eee22ba1
Fred <fred.gerner@icloud.com>
2020-07-04 11:33:31 +1000

    channels

--- chat.js
+++ chat.js
@@ -7,6 +7,7 @@ app.use(express.static(__dirname + '/public'));
 const expressServer = app.listen(3000);
 
 const io = socketIo(expressServer);
+
 io.on('connection', (socket) => {
     socket.emit('messageFromServer', {data: 'welcome from the server'});
     socket.on('messageToServer', (dataFromClient) => {
@@ -14,5 +15,12 @@ io.on('connection', (socket) => {
     });
     socket.on('newMessageToServer', (msg) => {
         io.emit('messageToClients', {text: msg.text});
-    })
+    });
+    setTimeout(() => {
+        io.of('/admin').emit('welcome', 'welcome to admin from main channel');
+    }, 2000);
+});
+io.of('/admin').on('connection', (socket) => {
+    console.log('admin shit');
+    io.of('/admin').emit('welcome', 'welcome to admin channel');
 });
--- public/script.js
+++ public/script.js
@@ -1,4 +1,16 @@
 const socket = io('http://localhost:3000');
+const socket2 = io('http://localhost:3000/admin');
+
+socket.on('connect', () => {
+    console.log(socket.id);
+});
+socket2.on('connect', () => {
+    console.log(socket2.id);
+});
+socket2.on('welcome', (msg) => {
+    console.log(msg);
+})
+
 socket.on('messageFromServer', (messageFromServer) => {
     console.log(messageFromServer);
     socket.emit('messageToServer', {data: 'Data from the client'});
