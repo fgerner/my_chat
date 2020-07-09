8e67779d9b4362291bd27a82c97121c1eeae60a3
Fred <fred.gerner@icloud.com>
2020-07-09 22:35:36 +1000

    added search box functionality.

--- public/joinRoom.js
+++ public/joinRoom.js
@@ -16,4 +16,15 @@ function joinRoom(roomName) {
         document.querySelector('.curr-room-num-users').innerHTML = `${numMembers}<span class="glyphicon glyphicon-user"></span>`;
         document.querySelector('.curr-room-text').innerText = `${roomName}`;
     })
+    let searchbox = document.querySelector('#search-box');
+    searchbox.addEventListener('input', (e) => {
+        let messages = Array.from(document.getElementsByClassName('message-text'));
+        messages.forEach((msg) => {
+            if (msg.innerText.toLowerCase().indexOf(e.target.value.toLowerCase()) === -1) {
+                msg.style.display = 'none';
+            } else {
+                msg.style.display = 'block';
+            }
+        })
+    })
 }
